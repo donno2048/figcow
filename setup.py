7da9829f3cdfd16366c6b63b53ed89c83ea8e566
Elisha Hollander <just4now666666@gmail.com>
2021-04-18 18:32:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='figcow',
-    version='1.0.0',
+    version='1.0.1',
     description='Python cowsay that looks like a figlet',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
