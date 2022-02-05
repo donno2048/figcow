9a375fafcd4ee9bdad3d4f4097abf914bf42601a
donno2048 <just4now666666@gmail.com>
2022-02-05 04:46:02 +0200

    add pipe

--- figcow/__main__.py
+++ figcow/__main__.py
@@ -1,4 +1,4 @@
 from . import cow
-from sys import argv
-def main(): print(cow(" ".join(argv[1:])))
+from sys import argv, stdin
+def main(): print(cow(stdin.read() if len(argv) == 1 else " ".join(argv[1:])))
 if __name__ == "__main__": main()
